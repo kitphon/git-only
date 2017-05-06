23c9486d391eabd59ddc0a98841e79e70de5e9c4
champ_kitphon <champ6069@gmail.com>
2017-05-06 16:20:08 +0700

    C1

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main{
 	public static void Main(){
-		System.out.println("Hello World NAJA");
+		System.out.println("Hello World NAJA A");
 		System.out,println("My Name is one1");
 		System.out.println("Hello Worldd");
 		System.out.println("Hello Earth1");
