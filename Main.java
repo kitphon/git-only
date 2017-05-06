77026818a375632c43e96d1b7031b9e4a02360e1
champ_kitphon <champ6069@gmail.com>
2017-05-06 13:35:58 +0700

    newesr

--- Main.java
+++ Main.java
@@ -5,11 +5,16 @@ class Main{
 		System.out.println("Hello Worldd");
 		System.out.println("Hello Earth1");
 		System.out.println("Hello Everything1");
-		system.out.println("Added01");
-		system.out.println("Added02");
-		system.out.println("Added03");
-		system.out.println("Added04");
-
+		System.out.println("Added01");
+		System.out.println("Added02");
+		System.out.println("Added03");
+		System.out.println("Added04");
+		for(int i =0; i < 10 ; i++){
+			System.out.println(i);
+			String myname = "nong" + i;
+			Person person = new Person();
+			System.out.println(person.toString());
+		}
 		System.out.println("Hello Meaw");
 	}
 }
