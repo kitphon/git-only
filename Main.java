0e8b571710c8b160c16a8df68ebf527ce6e4976b
champ_kitphon <champ6069@gmail.com>
2017-05-06 11:43:12 +0700

    commit +3

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
 class Main{
 	public static void Main(){
 		System.out.println("Hello World");
+		System.out,println("My Name is one1");
 		System.out.println("Hello Worldd");
-		System.out.println("Hello Everything");
+		System.out.println("Hello Earth1");
+		System.out.println("Hello Everything1");
+		System.out.println("Hello Meaw");
 	}
 }
